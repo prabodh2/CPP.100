6b39f7cb1921d71b676def45d35a66f62d69428f
prabodh2 <142776442+prabodh2@users.noreply.github.com>
2023-10-03 20:56:48 +0530

    Add files via upload

---
+++ 91.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Number of rows in the pattern
+  int rows = 5;
+
+  // Outer loop for each row
+  for (int i = rows; i >= 1; i--) {
+    // Inner loop for each column
+    for (int j = 1; j <= i; j++) {
+      // Print an asterisk
+      cout << "*";
+    }
+
+    // Print a newline after each row
+    cout << endl;
+  }
+
+  return 0;
+}

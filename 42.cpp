586849e9454e3a7a8a38ea7a815a60ace18c1ed1
prabodh2 <142776442+prabodh2@users.noreply.github.com>
2023-10-03 20:32:43 +0530

    Add files via upload

---
+++ 42.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double calculateSeriesSum(int n) {
+    double sum = 0.0;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            sum -= 1.0 / i; // Subtract 1/i if i is even
+        } else {
+            sum += 1.0 / i; // Add 1/i if i is odd
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    double sum = calculateSeriesSum(n);
+
+    cout << "Sum of the series is: " << sum << endl;
+
+    return 0;
+}

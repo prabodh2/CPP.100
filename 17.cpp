9295fd753b5505b4569bf46ebfae7282e9336d47
prabodh2 <142776442+prabodh2@users.noreply.github.com>
2023-10-03 20:26:51 +0530

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int square(int num) {
+    return num * num;
+}
+
+int cube(int num) {
+    return num * num * num;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int squareResult = square(num);
+    int cubeResult = cube(num);
+
+    cout << "Square of " << num << " is: " << squareResult << endl;
+    cout << "Cube of " << num << " is: " << cubeResult << endl;
+
+    return 0;
+}

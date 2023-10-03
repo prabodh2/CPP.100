d4867d259c7bb9d02f54d76de8c06599744953a2
prabodh2 <142776442+prabodh2@users.noreply.github.com>
2023-10-03 20:26:02 +0530

    Add files via upload

---
+++ 16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string str;
+
+    // Taking input from user
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    // Using the length() function to find the length of the string
+    int length = str.length();
+
+    // Displaying the length of the string
+    cout << "The length of the string is: " << length << endl;
+
+    return 0;
+}

6dd3ae8bf4f8a415319fc6013b66a3488cb1432c
prabodh2 <142776442+prabodh2@users.noreply.github.com>
2023-10-08 00:04:18 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main() {
     double a, b, c;
     
-    std::cout << "Enter coefficients (a, b, c): ";
-    std::cin >> a >> b >> c;
+    cout << "Enter coefficients (a, b, c): ";
+    cin >> a >> b >> c;
 
     double discriminant = b * b - 4 * a * c;
     
     if (discriminant > 0) {
         double root1 = (-b + sqrt(discriminant)) / (2 * a);
         double root2 = (-b - sqrt(discriminant)) / (2 * a);
-        std::cout << "Root 1: " << root1 << std::endl;
-        std::cout << "Root 2: " << root2 << std::endl;
+        cout << "Root 1: " << root1 << endl;
+        cout << "Root 2: " << root2 << std::endl;
     } else if (discriminant == 0) {
         double root1 = -b / (2 * a);
-        std::cout << "Root 1 and Root 2 are real and equal: " << root1 << std::endl;
+        cout << "Root 1 and Root 2 are real and equal: " << root1 << endl;
     } else {
         double realPart = -b / (2 * a);
         double imaginaryPart = sqrt(-discriminant) / (2 * a);
-        std::cout << "Root 1: " << realPart << " + " << imaginaryPart << "i" << std::endl;
-        std::cout << "Root 2: " << realPart << " - " << imaginaryPart << "i" << std::endl;
+        cout << "Root 1: " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "Root 2: " << realPart << " - " << imaginaryPart << "i" << endl;
     }
 
     return 0;

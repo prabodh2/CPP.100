7410d866f5caeed87fbb17c0eddf19f0ffd441b7
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-22 22:23:16 +0530

    Update 33.cpp

--- 33.cpp
+++ 33.cpp
@@ -5,9 +5,7 @@ int main() {
 
     std::cout << "Enter two numbers: ";
     std::cin >> num1 >> num2;
-
     int gcd = 1;  // Initialize the GCD to 1, as it's the minimum possible GCD
-
     // Find the GCD using a for loop
     for (int i = 1; i <= num1 && i <= num2; ++i) {
         if (num1 % i == 0 && num2 % i == 0) {
@@ -16,6 +14,5 @@ int main() {
     }
 
     std::cout << "The GCD of " << num1 << " and " << num2 << " is: " << gcd << std::endl;
-
     return 0;
 }

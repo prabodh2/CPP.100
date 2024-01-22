2023621c43b1fc79a083c0ef6e6325c9437aa331
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-22 22:23:41 +0530

    Update 34.cpp

--- 34.cpp
+++ 34.cpp
@@ -2,10 +2,8 @@
 
 int main() {
     int number, digit, sum = 0;
-
     std::cout << "Enter a number: ";
     std::cin >> number;
-
     int originalNumber = number;  // Store the original number for displaying later
 
     // Calculate the sum of digits using a do-while loop
@@ -14,9 +12,7 @@ int main() {
         sum += digit;        // Add the digit to the sum
         number /= 10;        // Remove the last digit
     } while (number != 0);
-
     // Display the result
     std::cout << "The sum of the digits of " << originalNumber << " is: " << sum << std::endl;
-
     return 0;
 }

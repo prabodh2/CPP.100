5e8adc70dcf7d307620ab511726eb703cb0db1e3
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-22 22:17:18 +0530

    Update 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 int main() {
     string str;
-
     // Taking input from user
     cout << "Enter a string: ";
     getline(cin, str);
-
     // Using the length() function to find the length of the string
     int length = str.length();
-
     // Displaying the length of the string
     cout << "The length of the string is: " << length << endl;
-
     return 0;
 }

93a05a86833b30f7292fafbc84f9aea8113f6528
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-22 22:25:37 +0530

    Update 93.cpp

--- 93.cpp
+++ 93.cpp
@@ -4,18 +4,14 @@ using namespace std;
 int main() {
     int n = 4; // Number of rows
     int current_char = 65; // ASCII value of 'A'
-
     // Loop for each row
     for(int i = 1; i <= n; i++) {
-
         // Loop for each column
         for(int j = 1; j <= i; j++) {
             cout << char(current_char) << " ";
             current_char++;
         }
-
         cout << endl; // Move to the next line after each row
     }
-
     return 0;
 }

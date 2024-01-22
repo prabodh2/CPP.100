69e0b593a9be408be744f0e189025ba9bf6efd4d
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-22 22:24:48 +0530

    Update 95.cpp

--- 95.cpp
+++ 95.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {
     int n = 4; // Number of rows
-
     // Loop for each row
     for(int i = 1; i <= n; i++) {
-
         // Loop for each column
         for(int j = 1; j <= i; j++) {
             if(j % 2 == 1) {
@@ -15,9 +13,7 @@ int main() {
                 cout << "0";
             }
         }
-
         cout << endl; // Move to the next line after each row
     }
-
     return 0;
 }

7c845a4cc5a3b5923faa4d2e9ad03caf50ed54ac
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-25 16:24:09 +0530

    Update _71HCF_Recursion.cpp

--- _71HCF_Recursion.cpp
+++ _71HCF_Recursion.cpp
@@ -12,7 +12,6 @@ int main() {
     cout << "\nGCD of "<< n1 << " and " << n2 << " is: " << hcf(n1,n2);
     return 0;
 }
-
 int hcf(int n1, int n2) {
     if (n2 != 0)
         return hcf(n2, n1 % n2);

ca57cf47c56f906e544590564415f04fd05ae9de
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-25 16:16:37 +0530

    Update 41.cpp

--- 41.cpp
+++ 41.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main()
 {
     int n1, n2, max;
-
     cout << "Enter two numbers: ";
     cin >> n1 >> n2;
-    
     // maximum value between n1 and n2 is stored in max
     max = (n1 > n2) ? n1 : n2;
 

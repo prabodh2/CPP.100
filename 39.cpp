ec2cfd39ec9554ac1de73a9c502f0d5c64b6b174
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-25 16:14:08 +0530

    Update 39.cpp

--- 39.cpp
+++ 39.cpp
@@ -6,13 +6,11 @@ int main() {
 
     cout << "Enter the number of rows for the pyramid: ";
     cin >> n;
-
     for (int i = 1; i <= n; ++i) {
         cout << string(n - i, ' '); // Print spaces
         for (int j = 1; j <= 2 * i - 1; ++j)
             cout << j; // Print numbers
         cout << endl;
     }
-
     return 0;
 }

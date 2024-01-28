5c55a65e6ad6949a6b92dab1fd02552821cf2d11
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-28 21:46:11 +0530

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 int main() {
     float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
     cout << "Enter coefficients a, b and c: ";

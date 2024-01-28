76e5f5461228a06ecfe283cc75e0e24cd6159c7c
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-28 21:48:25 +0530

    Update 41.cpp

--- 41.cpp
+++ 41.cpp
@@ -8,7 +8,6 @@ int main()
     cin >> n1 >> n2;
     // maximum value between n1 and n2 is stored in max
     max = (n1 > n2) ? n1 : n2;
-
     do
     {
         if (max % n1 == 0 && max % n2 == 0)
@@ -19,6 +18,5 @@ int main()
         else
             ++max;
     } while (true);
-    
     return 0;
 }

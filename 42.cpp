979ccebdbdd6f48d725709adba483d372eab5225
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-28 21:48:48 +0530

    Update 42.cpp

--- 42.cpp
+++ 42.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 double calculateSeriesSum(int n) {
     double sum = 0.0;
-
     for (int i = 1; i <= n; i++) {
         if (i % 2 == 0) {
             sum -= 1.0 / i; // Subtract 1/i if i is even
@@ -11,13 +10,10 @@ double calculateSeriesSum(int n) {
             sum += 1.0 / i; // Add 1/i if i is odd
         }
     }
-
     return sum;
 }
-
 int main() {
     int n;
-
     cout << "Enter the value of n: ";
     cin >> n;
     double sum = calculateSeriesSum(n);

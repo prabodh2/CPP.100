b2d667267c77ff83ab61ab2464a5ea5bd3027c26
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-30 20:25:44 +0530

    Update 91.cpp

--- 91.cpp
+++ 91.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
   // Number of rows in the pattern
   int rows = 5;
-
   // Outer loop for each row
   for (int i = rows; i >= 1; i--) {
     // Inner loop for each column
@@ -13,10 +11,8 @@ int main() {
       // Print an asterisk
       cout << "*";
     }
-
     // Print a newline after each row
     cout << endl;
   }
-
   return 0;
 }

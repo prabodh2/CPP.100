6d65502cefe67755a059f6e3e16b38a4a3bef40a
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-30 20:25:09 +0530

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -5,10 +5,8 @@ int main() {
 
     std::cout << "Enter the base number: ";
     std::cin >> base;
-
     std::cout << "Enter the exponent (a positive integer): ";
     std::cin >> exponent;
-
     if (exponent < 0) {
         std::cout << "Exponent should be a positive integer." << std::endl;
         return 1; // Exit the program with an error code

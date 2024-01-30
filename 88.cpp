08b7c17b6283bf603c89c7eee49435371b96de77
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-30 20:24:15 +0530

    Update 88.cpp

--- 88.cpp
+++ 88.cpp
@@ -6,13 +6,11 @@ int main() {
 
     cout << "Enter the number of rows: ";
     cin >> n;
-
     for (int i = 1; i <= n; i++) {
         // Print numbers from 1 to i
         for (int j = 1; j <= i; j++) {
             cout << j;
         }
-
         // Print numbers from i-1 down to 1
         for (int j = i - 1; j >= 1; j--) {
             cout << j;
@@ -20,6 +18,5 @@ int main() {
 
         cout << endl;
     }
-
     return 0;
 }

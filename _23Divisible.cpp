b5137a11e1ffbcd9887f01c712c1cbcc1d09cbb3
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-31 23:02:28 +0530

    Update _23Divisible.cpp

--- _23Divisible.cpp
+++ _23Divisible.cpp
@@ -13,34 +13,24 @@ int main()
         if (number % 5 == 0 && number % 11 == 0)
         {
             cout << number << " is divisible by 5 and 11" << endl;
-
         }
         else if (number < 0)
         {
             cout << "Please enter a positive number" << endl;
-
         }
         else
         {
             cout <<  number << " is not divisible by 5 and 11" << endl;
-
         }
-
         cout << "\nPress 1 to continue: \nPress 0 to exit: \nEnter your choice: ";
         cin >> choice;
-
         if (choice == 0)
         {
-            
             break;
         }
 
         cout << endl;
-        
-        
     }while(choice == 1);
-
     cout << "\nThankyou user the program is over :)";
-
     return 0;
 }

f4b3f28317be7e741e8c9719a0d2011b71c7a380
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-31 22:59:22 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -3,17 +3,14 @@
 int main() {
     int N;
     int sum = 0;
-
     // Prompt the user to enter N
     std::cout << "Enter a positive integer N: ";
     std::cin >> N;
-
     // Check if N is non-negative
     if (N <= 0) {
         std::cout << "Please enter a positive integer for N." << std::endl;
         return 1; // Exit the program with an error code
     }
-
     // Calculate the sum of even numbers from 1 to N
     for (int i = 2; i <= N; i += 2) {
         sum += i;

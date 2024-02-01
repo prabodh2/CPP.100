84d8e985a72f35d26b03021f4c20e7fbd4214a11
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-01 23:03:33 +0530

    Update 57.cpp

--- 57.cpp
+++ 57.cpp
@@ -14,7 +14,6 @@ int main() {
     for (int i = 0; i < size; ++i) {
         std::cin >> arr[i];
     }
-
     // Remove duplicates
     for (int i = 0; i < size; ++i) {
         for (int j = i + 1; j < size;) {
@@ -29,7 +28,6 @@ int main() {
             }
         }
     }
-
     std::cout << "Array with duplicates removed:" << std::endl;
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";

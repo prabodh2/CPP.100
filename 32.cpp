a5383f1fbf7031dba39d19dc8097c88d85717e21
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-01 23:02:34 +0530

    Update 32.cpp

--- 32.cpp
+++ 32.cpp
@@ -4,7 +4,6 @@ int main() {
     int number;
     std::cout << "Enter the number: ";
     std::cin >> number;
-
     std::cout << "Multiplication Table for " << number << ":\n";
     for (int i = 1; i <= 10; ++i) {
         std::cout << number << " x " << i << " = " << (number * i) << std::endl;

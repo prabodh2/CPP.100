fd23a14ed9c63f4bc7d7496763e9c197079c18b5
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-01 23:04:19 +0530

    Update 60.cpp

--- 60.cpp
+++ 60.cpp
@@ -5,9 +5,7 @@ int main() {
     int arr2[] = {3, 4, 5, 6, 7};
     int size1 = sizeof(arr1) / sizeof(arr1[0]);
     int size2 = sizeof(arr2) / sizeof(arr2[0]);
-
     std::cout << "Intersection of the two arrays: ";
-
     for (int i = 0; i < size1; ++i) {
         for (int j = 0; j < size2; ++j) {
             if (arr1[i] == arr2[j]) {

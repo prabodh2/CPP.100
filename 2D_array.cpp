9fdf8aaca4cc0a579e62eca577fed2d9e1304f8f
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-01 23:02:48 +0530

    Update 2D_array.cpp

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -6,8 +6,7 @@ int main() {
     // Define a 2D array
     int arr[3][3] ={{1, 2, 3},
                     {4, 5, 6},
-                    {7, 8, 9}};
-                                                                                                    
+                    {7, 8, 9}};                                                                                      
     int sum = 0;
     int n;
     cout << "Enter a value of n : ";

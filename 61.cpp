61dea0bb50160dafb937b597aa053b0466dd1184
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-01 23:04:32 +0530

    Update 61.cpp

--- 61.cpp
+++ 61.cpp
@@ -9,10 +9,8 @@ int main() {
     cin >> r;
     cout << "Enter the number of columns: ";
     cin >> c;
-
     // Declare a 2D matrix
     int matrix[r][c];
-
     // Input the matrix elements
     cout << "Enter the elements of the matrix: " << endl;
     for (int i = 0; i < r; i++) {
@@ -20,7 +18,6 @@ int main() {
             cin >> matrix[i][j];
         }
     }
-
     // Find the largest element in each row
     for (int i = 0; i < r; i++) {
         int maxElement = matrix[i][0];  // Assume the first element in the row is the maximum
